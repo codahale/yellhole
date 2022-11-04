796e9b418bd24641a7ba5363e513adc83a08a814
Coda Hale <coda.hale@gmail.com>
2022-11-04 08:00:07 -0600

    build(xtask): add DB commands

--- xtask/src/xtask.rs
+++ xtask/src/xtask.rs
@@ -15,19 +15,71 @@ struct XTask {
 enum Command {
     /// Format, build, test, and lint.
     CI,
+
+    /// Database commands.
+    DB {
+        #[clap(subcommand)]
+        cmd: DatabaseCommand,
+    },
+}
+
+#[derive(Debug, Subcommand)]
+enum DatabaseCommand {
+    Setup,
+    Reset,
+    Drop,
+    Migrate,
+    Prepare,
 }
 
 fn main() -> Result<()> {
-    let _ = XTask::parse();
+    let xtask = XTask::parse();
 
     let sh = Shell::new()?;
     sh.change_dir(project_root());
 
+    match xtask.cmd.unwrap_or(Command::CI) {
+        Command::CI => ci(&sh),
+        Command::DB { cmd } => match cmd {
+            DatabaseCommand::Setup => db_setup(&sh),
+            DatabaseCommand::Reset => db_reset(&sh),
+            DatabaseCommand::Drop => db_drop(&sh),
+            DatabaseCommand::Migrate => db_migrate(&sh),
+            DatabaseCommand::Prepare => db_prepare(&sh),
+        },
+    }
+}
+
+fn ci(sh: &Shell) -> Result<(), anyhow::Error> {
     cmd!(sh, "cargo fmt --check").run()?;
     cmd!(sh, "cargo build --all-targets --all-features").run()?;
     cmd!(sh, "cargo test --all-features").run()?;
     cmd!(sh, "cargo clippy --all-features --tests --benches").run()?;
+    Ok(())
+}
 
+fn db_setup(sh: &Shell) -> Result<(), anyhow::Error> {
+    cmd!(sh, "sqlx db setup").run()?;
+    Ok(())
+}
+
+fn db_reset(sh: &Shell) -> Result<(), anyhow::Error> {
+    cmd!(sh, "sqlx db reset").run()?;
+    Ok(())
+}
+
+fn db_drop(sh: &Shell) -> Result<(), anyhow::Error> {
+    cmd!(sh, "sqlx db drop").run()?;
+    Ok(())
+}
+
+fn db_migrate(sh: &Shell) -> Result<(), anyhow::Error> {
+    cmd!(sh, "sqlx migrate run").run()?;
+    Ok(())
+}
+
+fn db_prepare(sh: &Shell) -> Result<(), anyhow::Error> {
+    cmd!(sh, "cargo sqlx prepare -- --tests").run()?;
     Ok(())
 }
 
