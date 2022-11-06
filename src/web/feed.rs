636af6d1264ea7486cc660d12eda0f4c1f5fe1f5
Coda Hale <coda.hale@gmail.com>
2022-11-06 08:14:41 -0700

    refactor(feed): rename Index to Feed

--- src/web/feed.rs
+++ src/web/feed.rs
@@ -19,30 +19,30 @@ struct IndexOpts {
     n: Option<u16>,
 }
 
-async fn index(ctx: Extension<Context>, opts: Query<IndexOpts>) -> Result<Html<Index>, WebError> {
+async fn index(ctx: Extension<Context>, opts: Query<IndexOpts>) -> Result<Html<Feed>, WebError> {
     let notes = Note::most_recent(&ctx.db, opts.n.unwrap_or(100)).await?;
 
-    Ok(Html(Index { notes }))
+    Ok(Html(Feed { notes }))
 }
 
 async fn month(
     ctx: Extension<Context>,
     Path((year, month)): Path<(i32, u32)>,
-) -> Result<Html<Index>, WebError> {
+) -> Result<Html<Feed>, WebError> {
     let notes = Note::month(&ctx.db, year, month).await?.ok_or(WebError::NotFound)?;
-    Ok(Html(Index { notes }))
+    Ok(Html(Feed { notes }))
 }
 
 async fn single(
     ctx: Extension<Context>,
     Path(note_id): Path<String>,
-) -> Result<Html<Index>, WebError> {
+) -> Result<Html<Feed>, WebError> {
     let note = Note::by_id(&ctx.db, &note_id).await?.ok_or(WebError::NotFound)?;
-    Ok(Html(Index { notes: vec![note] }))
+    Ok(Html(Feed { notes: vec![note] }))
 }
 
 #[derive(Debug, Template)]
-#[template(path = "index.html")]
-struct Index {
+#[template(path = "feed.html")]
+struct Feed {
     notes: Vec<Note>,
 }
