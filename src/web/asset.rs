3e32262eba7f5fc962779dde074f7f0816d7a2ca
Coda Hale <coda.hale@gmail.com>
2022-11-07 19:44:19 -0700

    refactor(asset): use tower-http for headers

--- src/web/asset.rs
+++ src/web/asset.rs
@@ -1,32 +1,27 @@
 use std::path::Path;
 
 use axum::http::StatusCode;
-use axum::middleware::{self, Next};
-use axum::response::Response;
 use axum::routing::get_service;
 use axum::{http, Router};
 use tokio::io;
+use tower::ServiceBuilder;
 use tower_http::services::ServeDir;
+use tower_http::set_header::SetResponseHeaderLayer;
 
 pub fn router(images_dir: &Path) -> Router {
-    // TODO figure out how to handle 404 errors
-    let images = get_service(ServeDir::new(images_dir)).handle_error(io_error);
-    Router::new().nest("/images", images).route_layer(middleware::from_fn(cache_indefinitely))
+    let images = get_service(
+        ServiceBuilder::new()
+            .layer(SetResponseHeaderLayer::overriding(
+                http::header::CACHE_CONTROL,
+                http::HeaderValue::from_static("max-age=31536000,immutable"),
+            ))
+            .service(ServeDir::new(images_dir)),
+    )
+    .handle_error(io_error);
+    Router::new().nest("/images", images)
 }
 
 async fn io_error(err: io::Error) -> StatusCode {
     tracing::warn!(%err, "error handling static asset");
     StatusCode::INTERNAL_SERVER_ERROR
-}
-
-async fn cache_indefinitely<B>(
-    req: http::Request<B>,
-    next: Next<B>,
-) -> Result<Response, StatusCode> {
-    let mut resp = next.run(req).await;
-    resp.headers_mut().insert(
-        http::header::CACHE_CONTROL,
-        http::HeaderValue::from_static("max-age=31536000,immutable"),
-    );
-    Ok(resp)
 }
