9c4c1ee7ed050ed0099320efe3a1dc986ddea706
Coda Hale <coda.hale@gmail.com>
2022-11-29 19:51:58 -0700

    feat: enable debug logging for tower_http

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,12 @@ mod web;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // Configure tracing, defaulting to INFO except for sqlx, which is wild chatty.
+    // Configure tracing, defaulting to INFO except for sqlx, which is wild chatty, and tower_http,
+    // which is too terse.
     tracing_subscriber::registry()
-        .with(EnvFilter::new(std::env::var("RUST_LOG").unwrap_or_else(|_| "info,sqlx=warn".into())))
+        .with(EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "info,sqlx=warn,tower_http=debug".into()),
+        ))
         .with(tracing_subscriber::fmt::layer())
         .try_init()?;
 
