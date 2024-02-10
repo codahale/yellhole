ebb7a4620e84c3f872559614fd7b39b44fa07e35
Coda Hale <coda.hale@gmail.com>
2024-02-10 07:36:59 -0700

    refactor(feed): clean up Atom feed code

--- src/web/feed.rs
+++ src/web/feed.rs
@@ -131,64 +131,38 @@ async fn single(
 }
 
 async fn atom(State(state): State<AppState>) -> Result<Response, AppError> {
-    let mut out = Vec::<u8>::with_capacity(1024);
-    let mut xml = XmlWriter::new(&mut out);
-    xml.write_event(Event::Decl(BytesDecl::new("1.0", None, None))).map_err(anyhow::Error::new)?;
-
-    /*
-
-        <feed xmlns="http://www.w3.org/2005/Atom" xml:base="http://example.com/atom.xml">
-            <title>Yellhole</title>
-            <id>http://example.com/</id>
-            <updated>2024-02-10T02:39:07.277764+00:00</updated>
-            <author>
-                <name>Luther Blissett</name>
-            </author>
-            <link href="http://example.com/atom.xml" rel="alternate"/>
-            <subtitle>Obscurantist filth.</subtitle>
-
-            <entry>
-                <title>69b124f0-a4fa-40d0-83f4-06bc4213f3ca</title>
-                <id>http://example.com/note/69b124f0-a4fa-40d0-83f4-06bc4213f3ca</id>
-                <updated>1970-01-20T07:27:30.120+00:00</updated>
-                <link href="http://example.com/note/69b124f0-a4fa-40d0-83f4-06bc4213f3ca" rel="alternate"/>
-                <content type="html">&lt;p&gt;It’s a me, &lt;em&gt;Mario&lt;/em&gt;.&lt;/p&gt;
-    </content></entry>
-            <entry><title>c1449d6c-6b5b-4ce4-a4d7-98853562fbf1</title><id>http://example.com/note/c1449d6c-6b5b-4ce4-a4d7-98853562fbf1</id><updated>1970-01-20T06:42:58.651+00:00</updated><link href="http://example.com/note/c1449d6c-6b5b-4ce4-a4d7-98853562fbf1" rel="alternate"/><content type="html">&lt;h1&gt;Hello, it is a header.&lt;/h1&gt;
-    &lt;h2&gt;A Subheader&lt;/h2&gt;
-    </content></entry><entry><title>b0a2170c-5e91-42ad-aa1b-dabc3c6ea5b9</title><id>http://example.com/note/b0a2170c-5e91-42ad-aa1b-dabc3c6ea5b9</id><updated>1970-01-20T05:49:03.796+00:00</updated><link href="http://example.com/note/b0a2170c-5e91-42ad-aa1b-dabc3c6ea5b9" rel="alternate"/><content type="html">&lt;p&gt;Ok, I &lt;em&gt;guess&lt;/em&gt; this is fine.&lt;/p&gt;
-    </content></entry></feed>
-
-         */
-
     let notes = state.notes.most_recent(20).await?;
     let atom_url = filters::to_atom_url(&state.config.base_url).unwrap();
+    let mut xml = XmlWriter::new(Vec::<u8>::with_capacity(1024));
+    xml.write_event(Event::Decl(BytesDecl::new("1.0", None, None))).map_err(anyhow::Error::new)?;
     xml.create_element("feed")
         .with_attributes([
             ("xmlns", "http://www.w3.org/2005/Atom"),
             ("xml:base", atom_url.as_str()),
         ])
         .write_inner_content(|feed| -> anyhow::Result<()> {
-            feed.create_element("title").write_text_content(BytesText::new(&state.config.title))?;
-            feed.create_element("id")
+            feed.create_element("title")
+                .write_text_content(BytesText::new(&state.config.title))?
+                .create_element("id")
                 .write_text_content(BytesText::new(state.config.base_url.as_str()))?;
+
+            feed.create_element("author")
+                .write_inner_content(|author| -> anyhow::Result<()> {
+                    author
+                        .create_element("name")
+                        .write_text_content(BytesText::new(&state.config.author))?;
+                    Ok(())
+                })?
+                .create_element("link")
+                .with_attributes([("href", atom_url.as_str()), ("rel", "alternate")])
+                .write_empty()?
+                .create_element("subtitle")
+                .write_text_content(BytesText::new(&state.config.description))?;
 
             if !notes.is_empty() {
                 feed.create_element("updated")
                     .write_text_content(BytesText::new(&notes[0].created_at.format(&Rfc3339)?))?;
             }
-
-            feed.create_element("author").write_inner_content(|author| -> anyhow::Result<()> {
-                author
-                    .create_element("name")
-                    .write_text_content(BytesText::new(&state.config.author))?;
-                Ok(())
-            })?;
-            feed.create_element("link")
-                .with_attributes([("href", atom_url.as_str()), ("rel", "alternate")])
-                .write_empty()?;
-            feed.create_element("subtitle")
-                .write_text_content(BytesText::new(&state.config.description))?;
 
             for note in notes {
                 let url = filters::to_note_url(&note, &state.config.base_url).unwrap();
@@ -196,18 +170,14 @@ async fn atom(State(state): State<AppState>) -> Result<Response, AppError> {
                     |entry| -> anyhow::Result<()> {
                         entry
                             .create_element("title")
-                            .write_text_content(BytesText::new(&note.note_id.to_string()))?;
-                        entry
+                            .write_text_content(BytesText::new(&note.note_id.to_string()))?
                             .create_element("id")
-                            .write_text_content(BytesText::new(url.as_str()))?;
-                        entry.create_element("updated").write_text_content(BytesText::new(
-                            &note.created_at.format(&Rfc3339)?,
-                        ))?;
-                        entry
+                            .write_text_content(BytesText::new(url.as_str()))?
+                            .create_element("updated")
+                            .write_text_content(BytesText::new(&note.created_at.format(&Rfc3339)?))?
                             .create_element("link")
                             .with_attributes([("href", url.as_str()), ("rel", "alternate")])
-                            .write_empty()?;
-                        entry
+                            .write_empty()?
                             .create_element("content")
                             .with_attribute(("type", "html"))
                             .write_text_content(BytesText::new(&note.to_html()))?;
@@ -219,7 +189,7 @@ async fn atom(State(state): State<AppState>) -> Result<Response, AppError> {
             Ok(())
         })?;
 
-    Ok(([(http::header::CONTENT_TYPE, atom_xml())], out).into_response())
+    Ok(([(http::header::CONTENT_TYPE, atom_xml())], xml.into_inner()).into_response())
 }
 
 const fn atom_xml() -> http::HeaderValue {
@@ -262,11 +232,7 @@ mod tests {
             Some(atom_xml().as_bytes())
         );
 
-        let bytes = resp.bytes().await?;
-
-        println!("{}", String::from_utf8_lossy(&bytes));
-
-        let feed = Feed::read_from(Cursor::new(&bytes))?;
+        let feed = Feed::read_from(Cursor::new(&resp.bytes().await?))?;
         assert_eq!(
             feed.entries[0].content().unwrap().value().unwrap(),
             "<p>It’s a me, <em>Mario</em>.</p>\n"
