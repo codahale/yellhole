af37ba96dce0df4456cb92499b1f1ee43aa99a59
Coda Hale <coda.hale@gmail.com>
2025-06-26 19:04:59 -0600

    fix(images): resolve new clippy lint

--- src/services/images.rs
+++ src/services/images.rs
@@ -189,7 +189,7 @@ where
     E: Into<BoxError>,
 {
     // Convert the stream into an `AsyncRead`.
-    let body_with_io_error = stream.map_err(|err| io::Error::new(io::ErrorKind::Other, err));
+    let body_with_io_error = stream.map_err(|err| io::Error::other(err));
     let body_reader = StreamReader::new(body_with_io_error);
     futures::pin_mut!(body_reader);
 
